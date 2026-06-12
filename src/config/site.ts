c65b634bd8b2160ae3d98fccb1f051f69fcde2a7
prashant-1012 <prashant2009kr@gmail.com>
2026-06-12 14:59:39 +0530

    lead form mapped to sheet

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,19 +5,39 @@
 // The user has acknowledged the risk of storing these credentials in this manner.
 // Ideally, these credentials should be stored in environment variables for security.
 
+// export const siteConfig = {
+//   googleSheetId: '17eq2IuZgtyxGt78oEMrP791ehSW919UaUAtrEGpPFS0',
+//   googleServiceAccountCredentials: {
+//     type: 'service_account',
+//     project_id: 'anex-463306',
+//     private_key_id: 'ef28875c681727e0a033e4e4bbdaf070acae678b',
+//     private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCmFndRPWXzAMV2\nO9udhISRNccgfCPm2ctnENH5xdt5PwIS0YUli4uvRCTgx3mH7hIkDTXHec0/iz1K\nutly0Am7Eun/4YhcpnLQiunlqjS/c5JRHNvhuIA+RFZDne6rF3X8s3Mk0El4l45/\n85Q5mWOz+Finw8uMwu26iX0wmU37ahzUlT1V7h7UbwsWhS7LFZLkFVrcWJNrg1EL\nVlWQePH55/S+ypqlNXCvGbBf0GpJohS6A8eCXZxhA5gM0izJe/X+hhhi0s0sWAnc\nm6hqnl9gXjAz2xN3ae1RVNqIrL9+JO1Nxq8Y+j9F33UPN9wj5K5GZPmrBz6LdZ2X\nDr57N3x1AgMBAAECggEAI6aT4wzWWuRx/lCX8gZVfGP6Q8dR34YlipCKOd6WHz7+\nn3J47Ze4Gp16ewNmW16nZzSstA45l+e39tgHwATW6eCD+Qh8wEJ8R7ZhwYoY+3V/\n1UhoDFoRsQvzJl16sx/8lAsi7qaE9s01adKroPeDdsiaoxw8j5Dx/YXwqONnYU1L\nr4c/B3W+sVL0H7NRO1VWymO0qEWzEvyOZsNzujcIJS+0qveoiCt5RZq845+p5y5d\n9ygy0uQ0kRh0c4mBsvy4x55xgEpUCIqVDOG0TJTdAeAdHo8wuksn7N6euKtnCvbw\n0URQgzAkTCNC3t9jTJTJbzg1tX9gvTF0+Q9GDzZwywKBgQDPqwLTubpywPujDmXt\nXNwWqpldYMXxxcWjWMXCKd6zFp/4QV1RNmwWfpz4D3b42PdmoBTfZPUjvUEpcRf0\n7fN1J1lOpTCgIG6b6X7a5F3e7BsjDgW4O0ZMMVeDwPNnzHBhMCH0spZayDQKEx5R\nDAiVjyV8vKXfJ9c7oA/Do0IH5wKBgQDMvhPmMJL0OHjP5yhUGVRoKo2POLmh6sEe\ntdDVP0NMXASzseUTNLd5TnfaJXusS5CzJprqD3fJHWBMc2tvUzkEzlhJwl46WgPz\njP1tHY5Z4RMSjvwaj0KyzceI9wStpUa+pdWvR+yKmM+RapyC0qISb/kXJ70hsAdF\n1JjsZ1vdQwKBgB6QvxPGrGijeuYfAmWTW1mCtpwF2FyjjVljgYf4fazCUb1KcZ+4\ncORGsnfYXtx1osRaH3hZ9bLS8jpVSP9l3oWJyx1GNwf6fg04qtEw95gODFh2geUB\n5HDmekkMJ0dJ0eicGwNXHcYy3WX2NmobaVAKB6x6X7eet5ExF+DlsJxVAoGBAKB+\nO4ENAJUak3F9pTpPZMAG41Kqx7EqIVpecaZH4gxAcZsezxAAGfaoUQEasth7n6Ef\nmP3zRBXNxkgHjecFWMPwsHqZN14MY96N3bakHYtNCLNajDqRpFofPtOZtvwbe9pE\nir3M5po6cTNvW+KdFFjZl2vURcjbgMf3gCyIVRWbAoGAcKgk/KP5sJm1zxlOC2tk\nuVF8kjiUwTvVc+NdIj5qb4B1hzBipMc8WsQ6PR2Gq3czkBi5z9sd2wAAjbl439yb\nqtny4Earwkk1fw9iY3eYeYdAb2OAbFC38qDqIjc45R5VdFUsbcuGy4Do6TQR1Q2d\n+KYmzdeqdAgtSdkvGbwpiNI=\n-----END PRIVATE KEY-----',
+//     client_email: 'anex-543@anex-463306.iam.gserviceaccount.com',
+//     client_id: '114274487254443371071',
+//     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+//     token_uri: 'https://oauth2.googleapis.com/token',
+//     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+//     client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/anex-543%40anex-463306.iam.gserviceaccount.com',
+//     universe_domain: 'googleapis.com',
+//   },
+//   googleApiKey: 'AIzaSyAgGoMN7a9DsV492dvzTeWXE--OgXk63Yw', // Added for potential use with other Google APIs
+// };
+
+// ------------------ new below -----------------------
+
 export const siteConfig = {
-  googleSheetId: '17eq2IuZgtyxGt78oEMrP791ehSW919UaUAtrEGpPFS0',
+  googleSheetId: '1CIn0SrHsewcz6xWMt95lOoxCqrf76Uv_DzH8IoNNoFc',
   googleServiceAccountCredentials: {
     type: 'service_account',
-    project_id: 'anex-463306',
-    private_key_id: 'ef28875c681727e0a033e4e4bbdaf070acae678b',
-    private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCmFndRPWXzAMV2\nO9udhISRNccgfCPm2ctnENH5xdt5PwIS0YUli4uvRCTgx3mH7hIkDTXHec0/iz1K\nutly0Am7Eun/4YhcpnLQiunlqjS/c5JRHNvhuIA+RFZDne6rF3X8s3Mk0El4l45/\n85Q5mWOz+Finw8uMwu26iX0wmU37ahzUlT1V7h7UbwsWhS7LFZLkFVrcWJNrg1EL\nVlWQePH55/S+ypqlNXCvGbBf0GpJohS6A8eCXZxhA5gM0izJe/X+hhhi0s0sWAnc\nm6hqnl9gXjAz2xN3ae1RVNqIrL9+JO1Nxq8Y+j9F33UPN9wj5K5GZPmrBz6LdZ2X\nDr57N3x1AgMBAAECggEAI6aT4wzWWuRx/lCX8gZVfGP6Q8dR34YlipCKOd6WHz7+\nn3J47Ze4Gp16ewNmW16nZzSstA45l+e39tgHwATW6eCD+Qh8wEJ8R7ZhwYoY+3V/\n1UhoDFoRsQvzJl16sx/8lAsi7qaE9s01adKroPeDdsiaoxw8j5Dx/YXwqONnYU1L\nr4c/B3W+sVL0H7NRO1VWymO0qEWzEvyOZsNzujcIJS+0qveoiCt5RZq845+p5y5d\n9ygy0uQ0kRh0c4mBsvy4x55xgEpUCIqVDOG0TJTdAeAdHo8wuksn7N6euKtnCvbw\n0URQgzAkTCNC3t9jTJTJbzg1tX9gvTF0+Q9GDzZwywKBgQDPqwLTubpywPujDmXt\nXNwWqpldYMXxxcWjWMXCKd6zFp/4QV1RNmwWfpz4D3b42PdmoBTfZPUjvUEpcRf0\n7fN1J1lOpTCgIG6b6X7a5F3e7BsjDgW4O0ZMMVeDwPNnzHBhMCH0spZayDQKEx5R\nDAiVjyV8vKXfJ9c7oA/Do0IH5wKBgQDMvhPmMJL0OHjP5yhUGVRoKo2POLmh6sEe\ntdDVP0NMXASzseUTNLd5TnfaJXusS5CzJprqD3fJHWBMc2tvUzkEzlhJwl46WgPz\njP1tHY5Z4RMSjvwaj0KyzceI9wStpUa+pdWvR+yKmM+RapyC0qISb/kXJ70hsAdF\n1JjsZ1vdQwKBgB6QvxPGrGijeuYfAmWTW1mCtpwF2FyjjVljgYf4fazCUb1KcZ+4\ncORGsnfYXtx1osRaH3hZ9bLS8jpVSP9l3oWJyx1GNwf6fg04qtEw95gODFh2geUB\n5HDmekkMJ0dJ0eicGwNXHcYy3WX2NmobaVAKB6x6X7eet5ExF+DlsJxVAoGBAKB+\nO4ENAJUak3F9pTpPZMAG41Kqx7EqIVpecaZH4gxAcZsezxAAGfaoUQEasth7n6Ef\nmP3zRBXNxkgHjecFWMPwsHqZN14MY96N3bakHYtNCLNajDqRpFofPtOZtvwbe9pE\nir3M5po6cTNvW+KdFFjZl2vURcjbgMf3gCyIVRWbAoGAcKgk/KP5sJm1zxlOC2tk\nuVF8kjiUwTvVc+NdIj5qb4B1hzBipMc8WsQ6PR2Gq3czkBi5z9sd2wAAjbl439yb\nqtny4Earwkk1fw9iY3eYeYdAb2OAbFC38qDqIjc45R5VdFUsbcuGy4Do6TQR1Q2d\n+KYmzdeqdAgtSdkvGbwpiNI=\n-----END PRIVATE KEY-----',
-    client_email: 'anex-543@anex-463306.iam.gserviceaccount.com',
-    client_id: '114274487254443371071',
+    project_id: 'dbz-projects',
+    private_key_id: 'f0123f065798f82c6663f84f94ebcf6d5b4bb89c',
+    private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDdvPFsx0ClPrC5\n5qZQAHkKuC3YQGBX8lHKhJrbexavVJtzEpgXr8a6suK1s7ZwM50QeTsQL/8vEHEL\nB6+sgCIiIL2PZbdffGWQBX8lHbLp0n66Fy0paIHGO5eWzZwiL6oKN75ioWVJiWyt\nYUujG1GN0YD6i1mM7P2wkP4KlbMo5DZ553dRtcaOLeHJfPBRIZNd5PtFp9SbKaCJ\nt0s2nuwD5h0sRQW5Ir59ksw/7b39tPXaL2zMJFBOPabCUZ5Hpeq38akm1RomBOBC\nh01Xo4Jzex47qlOaB05Ea3e0aBx7KQbDgZh/mvLbBmKjUQ4Up/hYq47moj9Xp2NJ\nEMxhVXjxAgMBAAECggEASbgh+dzKyMVtYvjkbS+jDmXKVB2fCk41VCO/Cl5DXfo+\njkTtzN4zL2n+xpMfgfEe9FWhxdF3QRHxy018YdjdwEPrUcvq6bOFXpr9OP1FOMiz\nOjIvYRduIdY5ITltgc4f9bKvuMHY11h5ibERwQH5nLE89M80cA5wj229EA7SGDMc\n3l81wYFu3oxnX2w/J//Ca4S3IF2FiR7q8EQei65jXx0YAEVp9zx7V5cqJcOOg9D+\nx0aHvXl+gJFREWTIZEet+agg/uqooquDwq1bI0ybDraXf1XOK7vw9yCatSDyP7jW\n8UWG7dx7fh+1fCGEhCDppAgrjjRO9cK09Fa0eFRKKwKBgQDvyfxoAMeTUnxKzYgP\n6qvBZjekW64ypTv01D4oyuKIMHZtpQZ7TDA+Ex9s8HElTDarrhjsHYywRwPoZq5v\nycrjDwEHGWexnrkEO4wkzeg09hjk61GOdx1M1NtN8L2iSgfXJVJ+FrQ0qVf8YUF4\nxkVXjbOsgSsTABU3ex4Km/Cm2wKBgQDsuoznuWo4tWVTbXug6O/5EOI4sxwlvsWT\nU67Uv5pyD2k74yF+XneDxPejL+hzsLrAJ9rB1/n8G12zZjUrXZXSzTn881SQU1DK\nBOluSTgbClOleRoBUOBH3ELZRFq5cUAgOxo0Y4nv0mpYm3McJsFyouTC5d0Yu02X\n/Fyw5g3LIwKBgQC6P9FllvVtYKxaDbqZYlV2KscdSnjLrbwcNp7xYGUEE2BpWbzn\nGA6AqKnIHBpGNiRgxSFmhK06Xm6SnO7wtXR5WLYbpYf9VqOuvfHkxGqQeHZtmLoI\nRkzj2JFewJcK7yWX3s1sUoY2QTKuPXx1KsvBbbcZTlr+rxnLtj1nUChj8QKBgEZ7\n0OJZwXe7Ar1RVsL1OCYnUcPiCFpwr7EK9H4hwmF9ly6elOVkSR9xsO3koMh5cVEv\nTKH0pVjb6Q02PFR6G9bVBhP7e00xkVdlGP+eS1c5vugA465Kn0ymFz1LbdvTDW3L\nSyUYEgTW2JUjS6WxIF36hDRHboojkpnTKOH+zVKTAoGAFAXLHCOGt8GpqLO3YSMH\nrLj+9YHXYn71dvTLv0rPQogDgei8P9hh6XGMDvAu1tMHYfZEV5vsNVT00ACP1SZt\nprVHuHd01jh9F1eyK/OHkjdefbUF0dnpS9ENmw7UnggW7xG+B7/gay6PX0I71Vkc\n0ydfl4cDLwj/8NwhHGiwPEM=\n-----END PRIVATE KEY-----\n',
+    client_email: 'sheets-writer@dbz-projects.iam.gserviceaccount.com',
+    client_id: '116650321187973019880',
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
     token_uri: 'https://oauth2.googleapis.com/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-    client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/anex-543%40anex-463306.iam.gserviceaccount.com',
+    client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/sheets-writer%40dbz-projects.iam.gserviceaccount.com',
     universe_domain: 'googleapis.com',
   },
   googleApiKey: 'AIzaSyAgGoMN7a9DsV492dvzTeWXE--OgXk63Yw', // Added for potential use with other Google APIs
